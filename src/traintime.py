1cb5637c5c7f47aae831fb9e06773a761da7cc56
scetron <stephen.corry@networktocode.com>
2025-04-21 21:16:20 -0400

    adjust for mta connectivity

--- src/traintime.py
+++ src/traintime.py
@@ -43,6 +43,7 @@ def parse_gtfs_data(data):
     print(f"Current Time: {now.timestamp()}")
 
     # print("GTFS Realtime Data:")
+    departures = []
     for entity in feed.entity:
         if entity.HasField("trip_update"):
             # Display Only Hudson Line Trips
@@ -50,17 +51,45 @@ def parse_gtfs_data(data):
                 trip_stops = {stu.stop_id: stu for stu in entity.trip_update.stop_time_update}
                 if BEACON_STOP["id"] in trip_stops:
                     arrival_time = whenever.Instant.from_timestamp(trip_stops[BEACON_STOP["id"]].arrival.time)
+                    # arrival time at beacon
                     if arrival_time.subtract(hours=1) < now and arrival_time.add(hours=1) > now:
                         trip_update = entity.trip_update
-                        print(f"Trip ID: {trip_update.trip.trip_id}")
+                        # print(f"Trip ID: {trip_update.trip.trip_id}") # don't care about this, api/internal use only I think
                         trip_direction = "Northbound" if trip_update.stop_time_update[0].stop_id == GRAND_CENTRAL_STOP["id"] else "Southbound"
-                        print(f"  Direction: {trip_direction}")
-                        for stop_time_update in trip_update.stop_time_update:
-                            print(f"  Stop: {STOPS[stop_time_update.stop_id]}")
-                            if stop_time_update.arrival.time:
-                                arrival_time = whenever.ZonedDateTime.from_timestamp(stop_time_update.arrival.time, tz="America/New_York")
-                                # print(f"    Arrival Time: {arrival_time.local().time().format_common_iso()}") # 24-hour format
-                                print(f"    Arrival Time: {arrival_time.local().time().py_time().strftime('%-I:%M %p')}")
+                        service = {
+                            "destination": STOPS[trip_update.stop_time_update[-1].stop_id],
+                            "direction": trip_direction,
+                            "stopping_at": [],
+                            "departure_time": whenever.Instant.from_timestamp(trip_stops[BEACON_STOP["id"]].departure.time),
+                            "delay": trip_stops[BEACON_STOP["id"]].departure.delay,
+                        }
+                        
+                        if trip_direction == "Northbound":
+                            stops = []
+                            beacon_stop = False
+                            for stop in trip_update.stop_time_update:
+                                if beacon_stop:
+                                    stops.append(stop)
+                                if stop.stop_id == BEACON_STOP["id"]:
+                                    beacon_stop = True
+                            service["stopping_at"] = stops
+                        else:
+                            stops = []
+                            beacon_stop = False
+                            for stop in trip_update.stop_time_update:
+                                if not beacon_stop:
+                                    stops.append(stop)
+                                if stop.stop_id == BEACON_STOP["id"]:
+                                    break
+                            service["stopping_at"] = stops
+                        departures.append(service)
+    breakpoint()                    
+                        # # print(f"  Direction: {trip_direction}") 
+                        # for stop_time_update in trip_update.stop_time_update:
+                        #     print(f"  Stop: {STOPS[stop_time_update.stop_id]}")
+                        #     if stop_time_update.arrival.time:
+                        #         arrival_time = whenever.ZonedDateTime.from_timestamp(stop_time_update.arrival.time, tz="America/New_York")
+                        #         print(f"    Arrival Time: {arrival_time.local().time().py_time().strftime('%-I:%M %p')}")
 
 if __name__ == "__main__":
     raw_data = fetch_gtfs_data()
